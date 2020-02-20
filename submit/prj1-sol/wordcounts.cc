c2ae1f771ae93269b51e29e881512fe96e693c3d
Cat Tran <ctran20@binghamton.edu>
2020-02-19 22:03:33 -0500

    working project1

---
+++ submit/prj1-sol/wordcounts.cc
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <unordered_map>
+#include <vector>
+
+int main(int argc, char *argv[]) {
+
+	//No input error
+	if (argc == 1) {
+		std::cerr << "usage: " << argv[0]
+			      << " MAX_N_OUT MIN_WORD_LEN MAX_WORD_LEN FILE..." << std::endl;
+		std::exit(1);
+	}
+
+	std::ifstream inFile;
+
+	//Open input file
+	inFile.open("file1.txt");
+
+	std::string test;
+
+	while (inFile) {
+		inFile >> test;
+		std::cout << test << std::end;
+	}
+
+	//Close input file
+	inFile.close();
+}
