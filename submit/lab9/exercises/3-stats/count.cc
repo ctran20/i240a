e6eaae0b29f34edf2f653bf427e5975c1afe660b
Cat Tran <ctran20@binghamton.edu>
2020-04-02 13:41:40 -0400

    completing lab9

---
+++ submit/lab9/exercises/3-stats/count.cc
@@ -0,0 +1,22 @@
+#include "ops.hh"
+
+class Count : public Ops {
+
+public:
+	int swapCount = 0;
+	int compareCount = 0;
+
+	/** return < 0, == 0, > 0 if a < b, a == b, a > b */
+	virtual int compare(int a, int b) {
+		swapCount++;
+		return a - b;
+	};
+
+	/** swap a[i] and a[j] */
+	virtual void swap(int a[], int i, int j) {
+		compareCount++;
+		int t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	};
+};
